0614662d42eda087bff180a2c9bd4c47dd09cefe
Francisco Santos <francisco.santos@sinqia.com.br>
2020-11-14 15:42:45 -0300

    Inclusão das telas de cadastro e login

--- estimuloapp/lib/main.dart
+++ estimuloapp/lib/main.dart
@@ -1,117 +1,205 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  Login createState() {
+    return Login();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Login extends State<Home> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+
+  void validarLogin(String email, String senha) {
+    if (email == 'exemplo@email.com') {
+      if (senha == '1234') {
+        print("Login efetuado com sucesso!");
+        Navigator.push(context, null);
+      }
+    } else {
+      print('Email não cadastrado');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Scaffold(
+      appBar: AppBar(toolbarHeight: 0),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: null,
+                  color: Colors.blue,
+                  iconSize: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 80,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  'Entrar',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  'Entre com sua conta para obter uma melhor experiência.',
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                ),
+                SizedBox(height: 50),
+                TextField(
+                  decoration: InputDecoration(labelText: 'E-mail ou celular'),
+                  controller: emailController,
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  controller: senhaController,
+                  obscureText: true,
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  onPressed: null,
+                  child: Text('Recuperar minha senha',
+                      style: TextStyle(color: Colors.blue)),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: RaisedButton(
+                    onPressed: () {
+                      validarLogin(emailController.text, senhaController.text);
+                    },
+                    child: Text('Entrar',
+                        style: TextStyle(color: Colors.white, fontSize: 20)),
+                    color: Colors.blue,
+                    padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Cadastro extends State<Home> {
+  final TextEditingController razaoController = TextEditingController();
+  final TextEditingController numeroController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void validarLogin(String email, String senha) {
+    if (email == 'exemplo@email.com') {
+      if (senha == '1234') {
+        print("Login efetuado com sucesso!");
+        Navigator.push(context, null);
+      }
+    } else {
+      print('Email não cadastrado');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(toolbarHeight: 0),
+      body: Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: ListView(
+          shrinkWrap: true,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: null,
+                  color: Colors.blue,
+                  iconSize: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 80,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Cadastrar',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Realize seu cadastro de sua empresa.',
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Razão social'),
+                  controller: razaoController,
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  controller: emailController,
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Número de telefone ou celular'),
+                  controller: numeroController,
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  controller: senhaController,
+                  obscureText: true,
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: RaisedButton(
+                    onPressed: () {
+                      validarLogin(emailController.text, senhaController.text);
+                    },
+                    child: Text('Cadastrar',
+                        style: TextStyle(color: Colors.white, fontSize: 20)),
+                    color: Colors.blue,
+                    padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
