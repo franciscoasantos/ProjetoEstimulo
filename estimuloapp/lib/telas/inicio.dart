e8cd091a6de556cd956523d1898c255d888699a2
Francisco Santos <francisco.santos@sinqia.com.br>
2020-11-15 13:04:37 -0300

    Alteração da logo da pagina inicial

--- estimuloapp/lib/telas/inicio.dart
+++ estimuloapp/lib/telas/inicio.dart
@@ -26,7 +26,7 @@ class Inicio extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Image.asset(
-                  'assets/images/logo.png',
+                  'assets/images/estimulo2020.png',
                   width: 200,
                   fit: BoxFit.fill,
                 ),
